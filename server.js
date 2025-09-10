eafb4b9cc410f15e08bf8024bb0cf68b0668e728
Thulio Olimpio <167149365+thulioolimpio@users.noreply.github.com>
2025-09-10 13:15:35 -0300

    att

--- server.js
+++ server.js
@@ -6,21 +6,38 @@
  * Require Statements
  *************************/
 const express = require("express")
+const expressLayouts = require("express-ejs-layouts")
 const env = require("dotenv").config()
 const app = express()
 const static = require("./routes/static")
+const path = require('path');
+
+
+/* ***********************
+ * View Engine and Templates
+ *************************/
+app.set("view engine", "ejs")
+app.use(expressLayouts)
+app.set("layout", "./layouts/layout") // não está na raiz de views
 
 /* ***********************
  * Routes
  *************************/
 app.use(static)
+// Index route
+app.get("/", function(req, res) {
+  res.render("index", { title: "Home" })
+})
+
+app.use(express.static(path.join(__dirname, 'public')));
+
 
 /* ***********************
  * Local Server Information
  * Values from .env (environment) file
  *************************/
-const port = process.env.PORT
-const host = process.env.HOST
+const port = process.env.PORT || 5500
+const host = process.env.HOST || "localhost"
 
 /* ***********************
  * Log statement to confirm server operation
